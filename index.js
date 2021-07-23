2fd6f9994e6d551e55c785065ec8f5f0b74b6240
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-07-22 17:21:37 -0700

    complete tests

--- index.js
+++ index.js
@@ -182,8 +182,8 @@ class Car {
  *    // result is false
  * })
  */
-function isEvenNumberAsync(number) {
-  // ✨ implement
+async function isEvenNumberAsync(number) {
+  return number % 2 === 0 || false;
 }
 
 module.exports = {
